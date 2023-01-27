c27c4991c7a3c27aa0651dea1fc15c6f297ff849
Sergey <Sergey@yqndex.ru>
2023-01-27 21:56:16 +0300

    исправление ошибок

--- dz1_task2/Program.cs
+++ dz1_task2/Program.cs
@@ -1,13 +1,15 @@
-﻿Console.WriteLine("Введите три числа ");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите три числа ");
 int a = int.Parse(Console.ReadLine()!);
 int b = int.Parse(Console.ReadLine()!);
 int c = int.Parse(Console.ReadLine()!);
 int max = a;
-if (b > a)
-{
-  Console.WriteLine($"Число {b} максимальное");
-}
-if (c > a)
-{
-  Console.WriteLine($"Число {c} максимальное");
-}
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine($"Число {max} максимальное");
--- dz1_task4/Program.cs
+++ dz1_task4/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine("Введите число: ");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
 int n = int.Parse(Console.ReadLine()!);
 int count = 1;
 while (count <= n)
